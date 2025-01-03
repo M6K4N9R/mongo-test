a0bc552131ef3699beb72143257e8dcb162f56a6
M6K4N9R <zubchevskiy@gmail.com>
2025-01-03 14:16:24 +0100

    Set express server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+// init app & middleware
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('app is listening on port 3000');
+    
+})
+
+//routes
+
+app.get('/books', (req, res) => {
+    res.json({mssg: 'Welcome to the API'})
+})
